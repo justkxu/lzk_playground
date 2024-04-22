0a4adfe4864bb6488c1ed58b681736dbbfc9a861
Justin Kurpeik <justin.kurpeik@volkswagen.de>
2024-04-22 08:22:16 +0200

    Changed way of acces the df so pandas does not warn

--- main.py
+++ main.py
@@ -5,19 +5,17 @@ class Serializer:
     def __init__(self, document_path_netto: str, document_path_brutto: str):
         self.path_netto = document_path_netto
         self.path_brutto = document_path_brutto
-        self.data = {}
-
-    def extract_data(self):
-        self.data = pd.read_csv(self.path_netto, delimiter=";", thousands=".")
+        self.data_netto = pd.read_csv(self.path_netto, delimiter=";", thousands=".")
+        self.data_brutto = pd.read_csv(self.path_brutto, delimiter=";", thousands=".")
+        self.data = self.data_netto.astype("object")
 
     def run(self):
-        self.extract_data()
         for year, value in self.data.items():
             helper = 0
             if year != "Art":
                 sum = value.sum()
                 for zahl in value:
-                    self.data[year][helper] = round((zahl/sum)*100, 2)
+                    self.data.loc[helper, year] = round((zahl/sum)*100, 2)
                     helper += 1
         self.data['Anteil (Mittelwert)'] = self.data.iloc[:, 1:].mean(axis=1)
         self.txt_table(self.data, "result.txt")
